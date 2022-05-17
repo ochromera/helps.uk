8516dd9222f65774db6fe0bf34ba301c9dd2ec3b
NikGreh <ochromera@gmail.com>
2022-05-17 19:23:20 +0300

    create navbar menu

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import {FaBars, FaTimes } from 'react-icons/fa';
+import './navbar.css';
+
+
+const NavBar = () => {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+    return (
+        <div className="header">
+            <Link to="/"><h1>Meduza</h1></Link>
+            <ul className={click ? "nav-menu active" : "nav-menu" }>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+            <div className="hamburger" onClick={handleClick}>
+                {click? (<FaTimes size={20} style={{color: "#fff"}} />) :  <FaBars size={20} style={{color: "#fff"}}/> }
+
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
---
+++ src/components/PhotoBackground.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './photoBackground.css';
+
+
+const PhotoBackground = () => {
+return (
+    <div>
+
+    </div>
+)
+
+}
+
+
+export default PhotoBackground;
